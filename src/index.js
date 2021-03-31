894479159f3e220eeca4a59b4982de77e1d0ed8a
Vitaliy Gnylytsya <gnylytsya@gmail.com>
2021-03-31 17:01:52 +0300

    added props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const User = (props) =>  {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const name = "Max";
+
+
+const App = function () {
+  return (
+    <div>
+      <User name={name} age="28"/>
+      <User name="John" age="32"/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
